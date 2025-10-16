7745813e64c5a4a3cd47f3dea0f0534bb5459da5
Ankush Soni <ankushs@evtaar.com>
2025-10-16 21:11:13 +0530

    Update footet theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer id="Contact" className="w-full pb-10 bg-white dark:bg-black text-black dark:text-white" >
+    <footer id="Contact" className="w-full pb-10 bg-black text-white" >
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
@@ -16,11 +16,11 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw] text-black dark:text-white">
+        <h1 className="heading lg:max-w-[45vw]text-white">
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="text-gray-600 dark:text-gray-400 md:mt-10 my-5 text-center">
+        <p className="text-gray-400 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
@@ -33,7 +33,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light text-black dark:text-white">
+        <p className="md:text-base text-sm md:font-normal font-light text-white">
           Copyright © 2025 Ankush Soni
         </p>
 
@@ -41,7 +41,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-gray-800 border border-gray-600 hover:bg-gray-700"
             >
               <a href={info.url}>
                 <img src={info.img} alt="icons" width={20} height={20} />
